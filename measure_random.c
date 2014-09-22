579333bebeafb72327b70d55220f228fdbd7d7b8
Doug Richardson <dougie.richardson@gmail.com>
2014-09-22 15:55:07 +0000

    Add /dev/zero.

--- measure_random.c
+++ measure_random.c
@@ -11,7 +11,7 @@
 
 typedef int bucket_t[256];
 
-static void fill_bucket_from_fd(int fd, bucket_t bucket, int reads) {
+void fill_bucket_from_fd(int fd, bucket_t bucket, int reads) {
     FILE* fp = fdopen(fd, "r");
     if (fp == NULL) {
         perror("fdopen failed");
@@ -29,11 +29,12 @@ static void fill_bucket_from_fd(int fd, bucket_t bucket, int reads) {
         }
         bucket[c] += 1;
         putchar('.');
+        fflush(stdout);
     }
     putchar('\n');
 }
 
-static void fill_bucket_from_file(char const* file, bucket_t bucket, int reads) {
+void fill_bucket_from_file(char const* file, bucket_t bucket, int reads) {
     memset(bucket, 0, sizeof(bucket_t));
     int fd = open(file, O_RDONLY); 
     if (fd == -1) {
@@ -46,7 +47,7 @@ static void fill_bucket_from_file(char const* file, bucket_t bucket, int reads) 
     close(fd);
 }
 
-static void print_bucket(bucket_t bucket) {
+void print_bucket(bucket_t bucket) {
     for(int i = 0; i < 16; ++i) {
         printf("%3d: ", i * 16);
         int i_multiplied = i * 16;
@@ -60,7 +61,7 @@ static void print_bucket(bucket_t bucket) {
 unsigned int distance_from_expected(bucket_t b, int expected) {
     int result = 0;
     for(int i = 0; i < 256; ++i) {
-       result += abs(expected - b[i]); 
+        result += abs(expected - b[i]); 
     }
     return result;
 }
@@ -71,24 +72,25 @@ void print_bucket_report(char const* bucket_name, bucket_t bucket, int expected)
     print_bucket(bucket);
 }
 
+void run_test(char const *file, int samples, int expected_value) {
+    printf("Filling from %s.\n", file);
+    bucket_t b;
+    fill_bucket_from_file(file, b, samples);
+    print_bucket_report(file, b, expected_value);
+}
+
 int main(int argc, char** argv) {
+    // To take more samples, adjust expected value higher. I'm starting
+    // out with 1 because /dev/random blocks for so long and larger values
+    // on my system are a pain to work with.
     const int expected_value = 1;
     const int samples = 256 * expected_value;
-
     printf("Expected value %d\n", expected_value);
     printf("Samples per source %d\n", samples);
-
-    printf("Filling from /dev/urandom...\n");
-    bucket_t urandom_bucket;
-    fill_bucket_from_file("/dev/urandom", urandom_bucket, samples);
-
-    printf("Filling from /dev/random. This might block until you generate some events that contribute entropy.\n");
-    bucket_t random_bucket;
-    fill_bucket_from_file("/dev/random", random_bucket, samples);
-
-    print_bucket_report("urandom", urandom_bucket, expected_value);
-    print_bucket_report("random", random_bucket, expected_value);
-
+    run_test("/dev/zero", samples, expected_value);
+    run_test("/dev/urandom", samples, expected_value);
+    printf("/dev/random blocks longer than I'm willing to wait. Apparently, my systems don't generate enough noise.\n");
+    run_test("/dev/random", samples, expected_value);
     return 0;
 }
 
